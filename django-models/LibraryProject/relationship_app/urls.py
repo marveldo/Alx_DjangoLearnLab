94a11a08132f8a46e1de5d7cb90a63eb79ef38a8
utibe solomon <utibesolomon6@gmail.com>
2024-11-13 00:57:16 +0100

    Submisiion

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView,LogoutView
-from .views import list_books ,LibraryDetailView,UserRegisterView , add_book,edit_book,delete_book, register
+from .views import list_books ,LibraryDetailView,UserRegisterView , member_view, librarian_view , add_book,edit_book,delete_book, register
 from .admin_view import admin_view
-from .member_view import member_view
-from .librarian_view import librarian_view
+
 
 project = 'views.register'
 urlpatterns = [
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -23,6 +23,29 @@ def list_books(request):
     books = Book.objects.all()
     return render(request , 'relationship_app/list_books.html',context={'books':books})
 
+
+def is_member(user):
+    return user.userprofile.role != 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request , 'relationship_app/member_view.html')
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin' 
+        
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+def is_librarian(user):
+    return user.userprofile.role != 'Librarian' 
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
 class LibraryDetailView(DetailView):
 
     model = Library
