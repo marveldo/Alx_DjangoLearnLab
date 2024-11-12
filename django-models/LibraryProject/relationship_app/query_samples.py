6cda20798b0c21b6db0e31e34f72f7cfe1dc4194
utibe solomon <utibesolomon6@gmail.com>
2024-11-12 03:27:40 +0100

    Submission

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,11 +2,11 @@ from .models import Author , Book , Library
 author_name = 'John Doe'
 author = Author.objects.get(name=author_name)
 
-authors_books = author.books_set.all()
+authors_books = Book.objects.filter(author=author)
 print(authors_books)
 library_name = 'John Does library'
 library = Library.objects.get(name=library_name)
-libraries_books = library.books.all()
+libraries_books =   library.books.all()
 print(library)
 print(libraries_books)
 librarian = library.librarian
