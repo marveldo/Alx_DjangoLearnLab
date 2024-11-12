b575e8f1ed60e581de0cb3589ad48f3c9f04fd8a
utibe solomon <utibesolomon6@gmail.com>
2024-11-12 03:41:16 +0100

    Submission

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -41,7 +41,7 @@ class Rolechoices(models.TextChoices):
     ADMIN = 'Admin'
     LIBRARIAN = 'Librarian'
     MEMBER = 'Member'
-class Profile(models.Model):
+class UserProfile(models.Model):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=300, choices=Rolechoices.choices)
