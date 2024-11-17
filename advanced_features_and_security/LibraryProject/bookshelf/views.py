395016d2059e672507f3f102d6a7849077d3d4f5
utibe solomon <utibesolomon6@gmail.com>
2024-11-17 23:57:33 +0100

    Submitted

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,18 +5,18 @@ from .forms import ExampleForm,Book
 
 # Create your views here.
 listed = ["book_list", "books"]
-@permission_required('relationship_app.can_view', raise_exception=True)
+@permission_required('bookshelf.can_view', raise_exception=True)
 def view_author(request):
     return HttpResponse('Hello there')
 
-@permission_required('relationship_app.can_edit',raise_exception=True)
+@permission_required('bookshelf.can_edit',raise_exception=True)
 def edit_author(request):
     return HttpResponse('Edit author')
-@permission_required('relationship_app.can_delete', raise_exception=True)
+@permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_author(request):
     return HttpResponse('Delete author')
 
-@permission_required('relationship_app.can_create', raise_exception=True)
+@permission_required('bookshelf.can_create', raise_exception=True)
 def create_author(request):
     return HttpResponse('Can create')
 
