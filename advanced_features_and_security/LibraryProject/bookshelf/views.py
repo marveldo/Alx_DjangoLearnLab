b3aad1d1ec39215b46dea41ae1c28bc2c7b08ec2
utibe solomon <utibesolomon6@gmail.com>
2024-11-17 23:55:17 +0100

    Submitted

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
+from .forms import ExampleForm,Book
 
 # Create your views here.
 listed = ["book_list", "books"]
@@ -18,3 +19,15 @@ def delete_author(request):
 @permission_required('relationship_app.can_create', raise_exception=True)
 def create_author(request):
     return HttpResponse('Can create')
+
+def example_view(request):
+    form = ExampleForm()
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'bookshelf/form_example.html', context={'form' : form})
+
+def booklist(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', context={'books': books})
