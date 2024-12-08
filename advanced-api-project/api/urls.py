c7a1a4f9e2a44fa678ea1e3160a0c0371720879a
utibe solomon <utibesolomon6@gmail.com>
2024-12-08 23:51:00 +0100

    Suubmission

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from .views import ListView , CreateView , UpdateView , DetailView , DeleteView
 urlpattterns = [
     path('books/', ListView.as_view()),
     path('books/<int:pk>/', DetailView.as_view()),
-    path('books/', CreateView.as_view()),
-     path('books/<int:pk>/', UpdateView.as_view()),
-      path('books/<int:pk>/', DeleteView.as_view()),
+    path('books/create/', CreateView.as_view()),
+     path('books/update/<int:pk>/', UpdateView.as_view()),
+      path('books/delete/<int:pk>/', DeleteView.as_view()),
 ]
